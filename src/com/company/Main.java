a151cce0bd34f5f2f050740ea2dbbd66fe9bd467
[zemenuyar] <[zemenuyar@gmail.com]>
2018-10-03 17:11:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner Keybord=new Scanner(System.in);
+
+        System.out.println("Enter firstInital");
+        String firstinital=Keybord.nextLine();
+        System.out.println("Enter LastName");
+        String lastname=Keybord.nextLine();
+        System.out.println("Enter HouseNumber");
+        String  housenumber=Keybord.nextLine();
+        System.out.println("Enter StreetNumber");
+        String streetnumber=Keybord.nextLine();
+        System.out.println("Enter StreetType");
+        String streettype=Keybord.nextLine();
+        System.out.println("Enter City");
+        String entercity=Keybord.nextLine();
+
+    }
+}
